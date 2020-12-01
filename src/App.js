a2409be8aa33aeade0f2a28954735e28a859a333
jared Long <jlong4223@gmail.com>
2020-12-01 15:29:44 -0600

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import {useEffect, useState} from 'react'
 import './App.css';
 import Map from './components/Map/Map';
 
+
 function App () {
+  
+ // then we'll initialize our state passing in some blank data our app eventually needs 
+  const [appData, setAppData] = useState({
+    lat: null, 
+    lng: null
+  })
+
+  useEffect(()=>{
+    console.log('useEffect in the house')
+  })
+  
   return (
     <div className='App'>
       <Map />
-      <header className='App-header'>REACT WEATHER</header>
+      <header className='App-header'>REACT WEATHER
+       <button onClick={()=> setAppData({
+         lat: 32.8203525,
+         lng: -97.011731
+       })}>Set Weather Data</button>
+      </header>
     </div>
   );
 }
